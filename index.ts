3ff188d6897f99c6c6f529949dc2938aa78a588c
Juan Esteban Orozco <jeorozcob@gmail.com>
2021-11-16 15:19:43 -0500

    creación de base de datos relacional

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import connectDB from './db/db';
+
+import types from './graphql/types';
+import resolvers from './graphql/resolvers';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = new ApolloServer({
+  typeDefs: types,
+  resolvers: resolvers,
+});
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen({ port: process.env.PORT }, async () => {
+  await connectDB();
+  await server.start();
+  server.applyMiddleware({ app });
+
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
+});
