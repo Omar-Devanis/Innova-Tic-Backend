027e42c9e384927a6a0ef9f325f5ae64dbaaa394
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-11-20 20:47:39 -0500

    creacion token

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const { ApolloServer, gql } = require('apollo-server');
 const dotenv = require('dotenv')
 dotenv.config();
-const { MongoClient } = require('mongodb');
-const {DB_URL, DB_NAME}=process.env;
+const { MongoClient, ObjectId } = require('mongodb');
+const {DB_URL, DB_NAME, JWT_SECRET}=process.env;
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+const getToken = (user)=> jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: "30 days"});
+const getUserFromToken = async (token, db)=>{
+  
+  const tokenData =jwt.verify(token, JWT_SECRET);
+  return await db.collection("user").findOne({_id: ObjectId(tokenData.id)})
+}
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -38,6 +45,8 @@ type user{
   type Mutation{
     signUp(input:SignUpInput):AuthUser!
     signIn(input:SignInInput):AuthUser!
+
+    createTaskList(title: String!): TaskList!
   }
 
   input SignUpInput{
@@ -56,6 +65,22 @@ type user{
   type AuthUser{
     user:user!
     token: String!
+  }
+
+  type TaskList{
+    id: ID!
+    createdAt: String!
+    title: String!
+    progress: Float!
+    users: [user!]!
+    todos: [ToDo!]!
+  }
+  
+  type ToDo{
+    id : ID!
+    content: String!
+    isCompleted: Boolean!
+    taskList: TaskList!
   }
 `;
 
@@ -78,7 +103,7 @@ Mutation: {
 
   return{
     user:newUser,
-    token:"token",
+    token:getToken(newUser),
   }
 },
 
@@ -86,13 +111,15 @@ signIn: async(root, {input}, {db})=>{
   const user= await db.collection("user").findOne({mail: input.mail})
   const isPasswordCorrect = user && bcrypt.compareSync(input.password, user.password);
   if(!user || !isPasswordCorrect){
-    throw new Error("Credenciales no son correctas");
+    throw new Error("Credenciales incorrectas");
   }
   return{
     user,
-    token:"token"
+    token:getToken(user),
   }
-}
+},
+
+createTaskList: async(root,{title},{db, user})=> {}
 
 },
 user:{
@@ -110,13 +137,21 @@ user:{
     await client.connect();
     const db=client.db(DB_NAME)
 
-    const context={
-      db,
-    }
 
    // The ApolloServer constructor requires two parameters: your schema
   // definition and your set of resolvers.
-  const server = new ApolloServer({ typeDefs, resolvers, context});
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: async({req})=>{
+      const user = await getUserFromToken(req.headers.authorization, db);
+      console.log(user)
+      return {
+        db,
+        user,
+      }
+    },
+  });
 
   // The `listen` method launches a web server.
   server.listen().then(({ url }) => {
