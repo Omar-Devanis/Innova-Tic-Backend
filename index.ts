1b6a94f544ca10d73558daf755d2caf9326213f1
omar rodriguez <omardevanisrodriguez@gmail.com>
2021-11-23 19:44:41 -0500

    crear ingreso de usruarios basico

--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
 import conectarBD from "./db/db";
+import UserModel from "./models/user";
 
 const main = async () => {
     await conectarBD()
+
+    await UserModel.create({
+        correo:"omar@gmail.com",
+        identificacion:"12345",
+        nombre:"Omar",
+        apellido:"Rodriguez",
+    })
+    .then((u) =>{
+        console.log("Usuario creado", u);
+    })
+    .catch((e) => {
+        console.log("Error creando el usuario", e);
+    });
 };
 
 main();
---
+++ models/user.ts
@@ -0,0 +1,28 @@
+import {Schema, model} from "mongoose";
+
+const userSchema = new Schema ({
+    correo:{
+        type:String,
+        required: true,
+    },
+
+    identificacion:{
+        type:String,
+        required: true,
+        unique: true
+    },
+
+    nombre:{
+        type:String,
+        required: true,
+    },
+
+    apellido:{
+        type:String,
+        required: true,
+    },
+});
+
+const UserModel = model("user", userSchema);
+
+export default UserModel;
